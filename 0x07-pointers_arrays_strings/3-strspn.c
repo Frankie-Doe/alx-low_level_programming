d4f80b4f5581c9071a5492d81717de970ec80a9c
Frankie-Doe <lihandafranklin@yahoo.com>
2023-05-02 07:05:38 -0700

    Task4 rework

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * A function that gets the lengt of a prefix substring
  * _strspn - Entry point
+ * A function that gets the length of a prefix substring
  * @s: input
  * @accept: input
- * Return: Always 0 
+ * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
